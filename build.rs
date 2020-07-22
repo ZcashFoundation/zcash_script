c2252b2a077ef31452254e746221ecbff5cd2056
Jane Lusby <jlusby42@gmail.com>
2020-07-22 14:02:44 -0700

    initial working version on cc (#1)
    * initial working version on cc
    
    * add basic actions automation
    
    * checkout submodules
    
    * don't require bindgen generation
    
    * use newest zcash changes
    
    * pause work on ripping out sodium
    
    * push hacks to some version of the repo
    
    * fix path in build.rs
    
    * add job for testing different OSs
    
    * str4d is the fucking greatest
    
    * cleanup unused code
    
    * remove symlink
    
    * cleanup cruft
    
    * remove old automation files
    
    * Apply suggestions from code review
    
    Co-authored-by: Saleem Rashid <dev@saleemrashid.com>
    
    * add failure case test
    
    * always generate bindings
    
    * install llvm dep on windows
    
    * cleanup unused bindings
    
    * try messing with CI
    
    * use actual branch name
    
    * use correct remote for gitmodules
    
    * Update build.rs
    
    Co-authored-by: Saleem Rashid <dev@saleemrashid.com>
    
    Co-authored-by: Saleem Rashid <dev@saleemrashid.com>

--- build.rs
+++ build.rs
@@ -1,8 +1,33 @@
-extern crate cc;
+use color_eyre::eyre::{eyre, Context, Result};
+use std::{env, path::PathBuf};
 
-use std::env;
+fn bindgen_headers() -> Result<()> {
+    println!("cargo:rerun-if-changed=depend/zcash/src/script/zcashconsensus.h");
 
-fn main() {
+    let bindings = bindgen::Builder::default()
+        .header("depend/zcash/src/script/zcashconsensus.h")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        .map_err(|_| eyre!("Unable to generate bindings"))?;
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = env::var("OUT_DIR")?;
+    let out_path = PathBuf::from(out_path);
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .wrap_err("unable to write bindings")?;
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    bindgen_headers()?;
+
     // Check whether we can use 64-bit compilation
     let use_64bit_compilation = if env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap() == "64" {
         let check = cc::Build::new()
@@ -20,26 +45,33 @@ fn main() {
     let target = env::var("TARGET").expect("TARGET was not set");
     let is_big_endian = env::var("CARGO_CFG_TARGET_ENDIAN").expect("No endian is set") == "big";
     let mut base_config = cc::Build::new();
+
     base_config
         .cpp(true)
-        .include("depend/bitcoin/src")
-        .include("depend/bitcoin/src/secp256k1/include")
+        .include("depend/zcash/src")
+        .include("depend/zcash/src/rust/include/")
+        .include("depend/zcash/src/secp256k1/include")
+        .flag_if_supported("-Wno-implicit-fallthrough")
+        .flag_if_supported("-Wno-catch-value")
+        .flag_if_supported("-Wno-reorder")
+        .flag_if_supported("-Wno-deprecated-copy")
+        .define("HAVE_DECL_STRNLEN", "1")
         .define("__STDC_FORMAT_MACROS", None);
 
     // **Secp256k1**
     if !cfg!(feature = "external-secp") {
         base_config
-            .include("depend/bitcoin/src/secp256k1")
+            .include("depend/zcash/src/secp256k1")
             .flag_if_supported("-Wno-unused-function") // some ecmult stuff is defined but not used upstream
             .define("SECP256K1_BUILD", "1")
-            // Bitcoin core defines libsecp to *not* use libgmp.
+            // zcash core defines libsecp to *not* use libgmp.
             .define("USE_NUM_NONE", "1")
             .define("USE_FIELD_INV_BUILTIN", "1")
             .define("USE_SCALAR_INV_BUILTIN", "1")
             // Technically libconsensus doesn't require the recovery feautre, but `pubkey.cpp` does.
             .define("ENABLE_MODULE_RECOVERY", "1")
             // The actual libsecp256k1 C code.
-            .file("depend/bitcoin/src/secp256k1/src/secp256k1.c");
+            .file("depend/zcash/src/secp256k1/src/secp256k1.c");
 
         if is_big_endian {
             base_config.define("WORDS_BIGENDIAN", "1");
@@ -59,28 +91,31 @@ fn main() {
 
     let tool = base_config.get_compiler();
     if tool.is_like_msvc() {
-        base_config.flag("/std:c++14").flag("/wd4100");
+        base_config.flag("/std:c++17").flag("/wd4100");
     } else if tool.is_like_clang() || tool.is_like_gnu() {
-        base_config.flag("-std=c++11").flag("-Wno-unused-parameter");
+        base_config.flag("-std=c++17").flag("-Wno-unused-parameter");
     }
 
     if target.contains("windows") {
         base_config.define("WIN32", "1");
     }
+
     base_config
-        .file("depend/bitcoin/src/util/strencodings.cpp")
-        .file("depend/bitcoin/src/uint256.cpp")
-        .file("depend/bitcoin/src/pubkey.cpp")
-        .file("depend/bitcoin/src/hash.cpp")
-        .file("depend/bitcoin/src/primitives/transaction.cpp")
-        .file("depend/bitcoin/src/crypto/ripemd160.cpp")
-        .file("depend/bitcoin/src/crypto/sha1.cpp")
-        .file("depend/bitcoin/src/crypto/sha256.cpp")
-        .file("depend/bitcoin/src/crypto/sha512.cpp")
-        .file("depend/bitcoin/src/crypto/hmac_sha512.cpp")
-        .file("depend/bitcoin/src/script/bitcoinconsensus.cpp")
-        .file("depend/bitcoin/src/script/interpreter.cpp")
-        .file("depend/bitcoin/src/script/script.cpp")
-        .file("depend/bitcoin/src/script/script_error.cpp")
-        .compile("libbitcoinconsensus.a");
+        .file("depend/zcash/src/script/zcashconsensus.cpp")
+        .file("depend/zcash/src/utilstrencodings.cpp")
+        .file("depend/zcash/src/uint256.cpp")
+        .file("depend/zcash/src/pubkey.cpp")
+        .file("depend/zcash/src/hash.cpp")
+        .file("depend/zcash/src/primitives/transaction.cpp")
+        .file("depend/zcash/src/crypto/ripemd160.cpp")
+        .file("depend/zcash/src/crypto/sha1.cpp")
+        .file("depend/zcash/src/crypto/sha256.cpp")
+        .file("depend/zcash/src/crypto/sha512.cpp")
+        .file("depend/zcash/src/crypto/hmac_sha512.cpp")
+        .file("depend/zcash/src/script/interpreter.cpp")
+        .file("depend/zcash/src/script/script.cpp")
+        .file("depend/zcash/src/script/script_error.cpp")
+        .compile("libzcashconsensus.a");
+
+    Ok(())
 }
---
+++ src/blake2b.rs
@@ -0,0 +1,1 @@
+include!("../depend/zcash/src/rust/src/blake2b.rs");
