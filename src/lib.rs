337f5eb18259212bdefae15606a610ae66929b41
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-17 14:01:10 +0000

    Add missing FFI modules
    New FFI modules required for `libzcash_script`.

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,12 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+static mut ORCHARD_VK: Option<orchard::circuit::VerifyingKey> = None;
+
 mod blake2b;
+mod orchard_ffi;
+mod streams_ffi;
+mod transaction_ffi;
 
 #[cfg(test)]
 mod tests {
---
+++ src/transaction_ffi.rs
@@ -0,0 +1,1 @@
+include!("../depend/zcash/src/rust/src/transaction_ffi.rs");
