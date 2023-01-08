1d199407e585b9d1ef9261d0b1a2b7dbdc8a53b8
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-08 13:29:37 +0000

    Adding tarfile member sanitization to extractall()

--- depend/zcash/qa/rpc-tests/test_framework/util.py
+++ depend/zcash/qa/rpc-tests/test_framework/util.py
@@ -337,9 +337,47 @@ def initialize_chain(test_dir, num_nodes, cachedir, cache_behavior='current'):
             os.makedirs(to_dir)
             # Unzip the persisted Sprout config file
             with tarfile.open(os.path.join(sprout_cache_path, "chain_cache.tar.gz"), "r:gz") as tgz:
-                tgz.extractall(path = to_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(tgz, path=to_dir)
             with tarfile.open(os.path.join(sprout_cache_path, "node"+str(i)+"_wallet.tar.gz"), "r:gz") as tgz:
-                tgz.extractall(path = os.path.join(to_dir, "wallet.dat"))
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(tgz, path=os.path.join(to_dir,"wallet.dat"))
             with open(os.path.join(to_dir, 'cache_config.json'), "r", encoding="utf8") as cache_conf_file:
                 cache_conf = json.load(cache_conf_file)
                 # obtain the clock offset as a negative number of seconds
