b530b9cba18a07c5c473d73eec289cd16ecabeba
teor <teor@riseup.net>
2023-04-27 08:35:06 +1000

    Revert "Use a symlink for bridge.rs to avoid "expected outer doc comment" error TEMP: fix in zcashd"
    This reverts commit aeb650dc2ac1ed32f978fed9a7dbb407e571e60b.

--- src/bridge.rs
+++ src/bridge.rs
@@ -1,1 +1,7 @@
-../depend/zcash/src/rust/src/bridge.rs
+// workaround for "expected outer doc comment" error when including bridge.rs
+mod inner {
+    //! Try me
+    include!("../depend/zcash/src/rust/src/bridge.rs");
+}
+
+pub use inner::*;
