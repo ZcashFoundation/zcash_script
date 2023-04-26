44794ab95e88e7a980704a606505654f48e5e5fa
teor <teor@riseup.net>
2023-04-27 08:35:53 +1000

    Use include!() for bridge.rs

--- src/bridge.rs
+++ src/bridge.rs
@@ -1,7 +1,1 @@
-// workaround for "expected outer doc comment" error when including bridge.rs
-mod inner {
-    //! Try me
-    include!("../depend/zcash/src/rust/src/bridge.rs");
-}
-
-pub use inner::*;
+include!("../depend/zcash/src/rust/src/bridge.rs");
