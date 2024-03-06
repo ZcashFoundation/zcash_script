438369d27151f2b05dd40a972c3b991a09118500
Conrado Gouvea <conradoplg@gmail.com>
2024-03-06 17:47:54 -0300

    created zcash_script_verify_prehashed; compiles

--- depend/zcash/src/script/zcash_script.h
+++ depend/zcash/src/script/zcash_script.h
@@ -133,6 +133,21 @@ EXPORT_SYMBOL int zcash_script_verify(
     uint32_t consensusBranchId,
     zcash_script_error* err);
 
+EXPORT_SYMBOL int zcash_script_verify_prehashed(
+    const unsigned char* sighash,
+    unsigned int sighashLen,
+    int64_t nLockTime,
+    uint8_t isFinal,
+    const unsigned char* scriptPubKey,
+    unsigned int scriptPubKeyLen,
+    const unsigned char* scriptSig,
+    unsigned int scriptSigLen,
+    int64_t amount,
+    unsigned int nIn,
+    unsigned int flags,
+    uint32_t consensusBranchId,
+    zcash_script_error* err);
+
 /// Returns 1 if the input nIn of the serialized transaction pointed to by
 /// txTo correctly spends the matching output in allPrevOutputs under
 /// the additional constraints specified by flags. Must be used for V5 transactions;
