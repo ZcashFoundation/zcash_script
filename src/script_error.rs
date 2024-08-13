b8d0c8703fab241e9d731fef3b8174ace120842d
Greg Pfeil <greg@technomadic.org>
2024-08-12 19:02:12 -0600

    Connected the new API, but fails

--- src/script_error.rs
+++ src/script_error.rs
@@ -1,19 +1,48 @@
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
+#[repr(i32)]
 pub enum ScriptError {
+    Ok = 0,
+    UnknownError,
+    EvalFalse,
     OpReturn,
 
+    // Max sizes
     ScriptSize,
     PushSize,
     OpCount,
     StackSize,
+    SigCount,
+    PubKeyCount,
 
+    // Failed verify operations
+    VERIFY,
+    EQUALVERIFY,
+    CHECKMULTISIGVERIFY,
+    CHECKSIGVERIFY,
+    NUMEQUALVERIFY,
+
+    // Logical/Format/Canonical errors
     BadOpcode,
     DisabledOpcode,
     InvalidStackOperation,
+    InvalidAltstackOperation,
     UnbalancedConditional,
 
-    MinimalData,
+    // OP_CHECKLOCKTIMEVERIFY
+    NegativeLockTime,
+    UnsatisfiedLockTime,
 
+    // BIP62
+    SigHashtype,
+    SigDER,
+    MinimalData,
+    SigPushOnly,
+    SigHighS,
+    SigNullDummy,
+    PubKeyType,
+    CleanStack,
+
+    // softfork safeness
     DiscourageUpgradableNOPs,
 
     ReadError {
