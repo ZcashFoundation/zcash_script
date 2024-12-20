c904ba3ce47188b499ac4eeb8432097f6774627b
Greg Pfeil <greg@technomadic.org>
2024-12-20 14:52:20 -0700

    Partition `Operation` enum
    This splits `Operation` into three separate enums – `Control` (for
    if/else, which get executed regardless of `vexec` and disabled
    operations, which always fail, regardless of `vexec`), `Normal`, for
    operations that respect `vexec`, and `Unknown` for undefined opcodes
    which only fail if they’re on an active branch. This is done so that the
    evaluator can be split on the same lines.
    
    __NB__: I strongly recommend ignoring whitespace changes when reviewing
            this commit.
    
    (cherry picked from commit f97b92c9b485ad1d3ca233749a1b5cd864ad800d)

--- src/lib.rs
+++ src/lib.rs
@@ -300,7 +300,7 @@ pub mod testing {
     use super::*;
     use crate::{
         interpreter::{State, StepFn},
-        script::{Operation, Script},
+        script::{Normal, Script},
         test_vectors::TestVector,
     };
 
@@ -334,7 +334,7 @@ pub mod testing {
             payload: &mut T::Payload,
         ) -> Result<&'a [u8], ScriptError> {
             self.0.call(
-                if pc[0] == Operation::OP_EQUAL.into() {
+                if pc[0] == Normal::OP_EQUAL.into() {
                     &pc[1..]
                 } else {
                     pc
